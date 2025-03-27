4fbecdac9c24b14474ae4cd0ff7eb4396a088932
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-03-27 18:20:35 -0300

    attempt to display

--- src/main.tsx
+++ src/main.tsx
@@ -2,45 +2,60 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {Canvas, useFrame} from '@react-three/fiber'
 import getContainersOfPairs from './slopeGenerator'
-import { Fraction } from './Fraction'
+import {Fraction} from './Fraction'
+import {useState} from 'react'
 
+// TODO-display this 
+let jsxContainer = (<Canvas>
+		<mesh >
+			<boxGeometry args={[1, 1, 1]} />
+			<meshBasicMaterial color={'#1D7169'} />
+		</mesh>
+	</Canvas>)
 
-interface xyz {
- [key: string]: number | undefined,
-	x?: number,
-	y?: number,
-	z?: number,
-}
+const root = createRoot(document.getElementById('root')!)
+
 
 function generateSlope(textGraph: string, amountOfVarialbe: string) {
-	let container: xyz[] = getContainersOfPairs(textGraph, Number(amountOfVarialbe));
-	console.log(container)
+	let container: any[] = getContainersOfPairs(textGraph, Number(amountOfVarialbe));
+
+	let jsxContainer = (<Canvas>{container.map(e => {
+		return (<mesh position={[e['x'], e['z'], e['y']]}>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshBasicMaterial color={'#1D7169'} />
+		</mesh>)
+	})}</Canvas>)
+
+	render()
+	console.log(jsxContainer)
 }
 
-createRoot(document.getElementById('root')!).render(
-	(<><header><h1>3D SLOPERO</h1></header>
-		<main>
-			<fieldset>
-				<legend>
-					options for the graph
-				</legend>
-				<label htmlFor="equationText" >Write equation to graph</label>
-				<input id='equationText' type="text" placeholder="y = 2x + 6z" />
-				<label htmlFor="variableAmount" >amount of variable</label>
-				<input type="number" name="variableAmount" id="variableAmount" placeholder='10' />
-				<button onClick={() => {
-					let equationText: HTMLInputElement = document.querySelector('#equationText')!
-					let variableAmount: HTMLInputElement = document.querySelector('#variableAmount')!
-					generateSlope(equationText.value, variableAmount.value)
-				}}>apply</button>
-			</fieldset>
-			<Canvas>
+function render() {
 
-				<mesh>
-					<boxGeometry args={[1, 1, 1]} />
-					<meshBasicMaterial color={'#1D7169'} />
-				</mesh>
-			</Canvas>
-		</main>
-	</>)
-)
+
+	root.render(
+		(<><header><h1>3D SLOPERO</h1></header>
+			<main>
+				<fieldset>
+					<legend>
+						options for the graph
+					</legend>
+					<label htmlFor="equationText" >Write equation to graph</label>
+					<input id='equationText' type="text" placeholder="y = 2x + 6z" />
+					<label htmlFor="variableAmount" >amount of variable</label>
+					<input type="number" name="variableAmount" id="variableAmount" placeholder='10' />
+					<button onClick={() => {
+						let equationText: HTMLInputElement = document.querySelector('#equationText')!
+						let variableAmount: HTMLInputElement = document.querySelector('#variableAmount')!
+						generateSlope(equationText.value, variableAmount.value)
+					}}>apply</button>
+				</fieldset>
+				<Canvas>
+
+					{jsxContainer}
+				</Canvas>
+			</main >
+		</>)
+	)
+}
+render()
