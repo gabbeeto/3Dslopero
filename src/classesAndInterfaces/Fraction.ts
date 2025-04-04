774ac22527195652abf5d266ddece90d6e68b311
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-04-04 17:53:58 -0300

    making constant to variables that are not reused

--- src/classesAndInterfaces/Fraction.ts
+++ src/classesAndInterfaces/Fraction.ts
@@ -1,102 +1,128 @@
 export class Fraction {
-	Numerator: number = 1;
-	Denominator: number = 1;
+  Numerator: number = 1;
+  Denominator: number = 1;
 
-	constructor(content: string = "empty") {
-		if (content == "empty") {
-			this.Numerator = 0
-			this.Denominator = 0;
-		}
+  constructor(content: string = "empty") {
+    if (content == "empty") {
+      this.Numerator = 0;
+      this.Denominator = 0;
+    }
 
-		const numberpattern = '(-| -|- )?[0-9]+';
-		const divisionSymbol: string = "(\\\\|\\/|%)"
+    const numberpattern = "(-| -|- )?[0-9]+";
+    const divisionSymbol: string = "(\\\\|\\/|%)";
 
-		let divisionSymbolIsFound: boolean = content.search(new RegExp(`${divisionSymbol}`)) != -1
-		let hasParenthesis: boolean = content.includes("(");
-		let itsAFraction: Boolean = hasParenthesis && divisionSymbolIsFound;
-		if (itsAFraction) {
-			let [numeratorText, _symbol, denominatorText] = content.split(new RegExp(`${divisionSymbol}`, 'u'));
-			let numberatorTextButShort: string = numeratorText.match(new RegExp(numberpattern))![0];
-			let denominatorTextButShort: string = denominatorText.match(new RegExp(numberpattern))![0];
+    const divisionSymbolIsFound: boolean =
+      content.search(new RegExp(`${divisionSymbol}`)) != -1;
+    const hasParenthesis: boolean = content.includes("(");
+    const itsAFraction: boolean = hasParenthesis && divisionSymbolIsFound;
+    if (itsAFraction) {
+      const [numeratorText, , denominatorText] = content.split(
+        new RegExp(`${divisionSymbol}`, "u"),
+      );
+      const numberatorTextButShort: string = numeratorText.match(
+        new RegExp(numberpattern),
+      )![0];
+      const denominatorTextButShort: string = denominatorText.match(
+        new RegExp(numberpattern),
+      )![0];
 
-			let numberatorTextWithoutSpaces: string = numberatorTextButShort.split(" ").join("")
-			let denominatorTextWithoutSpaces: string = denominatorTextButShort.split(" ").join("")
-			this.Numerator = Number(numberatorTextWithoutSpaces)
-			this.Denominator = Number(denominatorTextWithoutSpaces)
+      const numberatorTextWithoutSpaces: string = numberatorTextButShort
+        .split(" ")
+        .join("");
+      const denominatorTextWithoutSpaces: string = denominatorTextButShort
+        .split(" ")
+        .join("");
+      this.Numerator = Number(numberatorTextWithoutSpaces);
+      this.Denominator = Number(denominatorTextWithoutSpaces);
 
-			let bothValuesAreNegative: boolean = this.Numerator < 0 && this.Denominator < 0
-			let fractionFlipled = this.Numerator >= 0 && this.Denominator < 0;
-			if (bothValuesAreNegative || fractionFlipled) {
-				this.Numerator = invertNumber(this.Numerator)
-				this.Denominator = invertNumber(this.Denominator)
-			}
+      const bothValuesAreNegative: boolean =
+        this.Numerator < 0 && this.Denominator < 0;
+      const fractionFlipled = this.Numerator >= 0 && this.Denominator < 0;
+      if (bothValuesAreNegative || fractionFlipled) {
+        this.Numerator = invertNumber(this.Numerator);
+        this.Denominator = invertNumber(this.Denominator);
+      }
 
-			let contentIsNegative: boolean = content[0] == '-'
-			if (contentIsNegative) {
-				this.Numerator = invertNumber(this.Numerator)
-			}
-		}
-		else if (content != "empty") {
+      const contentIsNegative: boolean = content[0] == "-";
+      if (contentIsNegative) {
+        this.Numerator = invertNumber(this.Numerator);
+      }
+    } else if (content != "empty") {
+      if (hasParenthesis) {
+        const allTheNumbers = [
+          ...content.matchAll(new RegExp(numberpattern, "g"))!,
+        ];
+        const newNumbers = allTheNumbers
+          .map((e) => e[0])
+          .map((e) => e.split(" ").join(""))
+          .map((e) => Number(e));
+        let newValue = 0;
+        newNumbers.forEach((e) => {
+          newValue += e;
+        });
+        this.Numerator = newValue;
+      } else {
+        let numberatorTextWithoutSpaces: string = content.match(
+          new RegExp(numberpattern),
+        )![0];
+        numberatorTextWithoutSpaces = numberatorTextWithoutSpaces
+          .split(" ")
+          .join("");
+        this.Numerator = Number(numberatorTextWithoutSpaces);
+      }
+    }
 
-			if (hasParenthesis) {
-				let allTheNumbers = [...content.matchAll(new RegExp(numberpattern, 'g'))!]
-				let newNumbers = allTheNumbers.map(e => e[0])
-					.map(e => e.split(" ").join(""))
-					.map(e => Number(e))
-				let newValue = 0
-				newNumbers.forEach(e => {newValue += e})
-				this.Numerator = newValue
-			}
-			else {
-				let numberatorTextWithoutSpaces: string = content.match(new RegExp(numberpattern))![0]
-				numberatorTextWithoutSpaces = numberatorTextWithoutSpaces.split(" ").join("")
-				this.Numerator = Number(numberatorTextWithoutSpaces);
-			}
+    const NumAndDen = simplify(
+      this.Numerator,
+      this.Denominator,
+      this.Numerator > this.Denominator ? this.Numerator : this.Denominator,
+    );
 
-		}
+    this.Numerator = NumAndDen[0];
+    this.Denominator = NumAndDen[1];
+  }
 
-		let NumAndDen = simplify(this.Numerator, this.Denominator, this.Numerator > this.Denominator ? this.Numerator : this.Denominator)
-
-		this.Numerator = NumAndDen[0];
-		this.Denominator = NumAndDen[1];
-	}
-
-
-turnIntoDecimal(){
-	if (this.Numerator == 0){
-		return this.Numerator
-	}
-	else{
-	return this.Numerator / this.Denominator
-	}
-
-}
-
+  turnIntoDecimal() {
+    if (this.Numerator == 0) {
+      return this.Numerator;
+    } else {
+      return this.Numerator / this.Denominator;
+    }
+  }
 }
 function invertNumber(number: number): number {
-	return number * -1;
-
+  return number * -1;
 }
 
-function simplify(numerator: number, denominator: number, highestNumber: number, changingNumber: number = 2): number[] {
-
-	if (changingNumber >= highestNumber) {
-		return [numerator, denominator];
-	}
-	else {
-		let numeritorIsDivisibleByChangingNumber: boolean = numerator % changingNumber == 0;
-		let denominatorIsDivisibleByChangingNumber: boolean = denominator % changingNumber == 0;
-		if (numeritorIsDivisibleByChangingNumber && denominatorIsDivisibleByChangingNumber) {
-			numerator /= changingNumber
-			denominator /= changingNumber
-			highestNumber = numerator > denominator ? numerator : denominator;
-			// this resets divisible number so it can check again if the same divisible number is repeated
-			return simplify(numerator, denominator, highestNumber, 2)
-		}
-		else {
-			return simplify(numerator, denominator, highestNumber, changingNumber + 1)
-		}
-
-	}
-
+function simplify(
+  numerator: number,
+  denominator: number,
+  highestNumber: number,
+  changingNumber: number = 2,
+): number[] {
+  if (changingNumber >= highestNumber) {
+    return [numerator, denominator];
+  } else {
+    const numeritorIsDivisibleByChangingNumber: boolean =
+      numerator % changingNumber == 0;
+    const denominatorIsDivisibleByChangingNumber: boolean =
+      denominator % changingNumber == 0;
+    if (
+      numeritorIsDivisibleByChangingNumber &&
+      denominatorIsDivisibleByChangingNumber
+    ) {
+      numerator /= changingNumber;
+      denominator /= changingNumber;
+      highestNumber = numerator > denominator ? numerator : denominator;
+      // this resets divisible number so it can check again if the same divisible number is repeated
+      return simplify(numerator, denominator, highestNumber, 2);
+    } else {
+      return simplify(
+        numerator,
+        denominator,
+        highestNumber,
+        changingNumber + 1,
+      );
+    }
+  }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,52 @@
-import {createRoot} from 'react-dom/client'
-import './index.css'
-import generateSlope from './displayGraph/applyButton'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import generateSlope from "./displayGraph/applyButton";
 
-window.jsxContainer = (<></>)
-window.xyzContainer = [<>equation not generated</>]
+window.jsxContainer = <></>;
+window.xyzContainer = [<>equation not generated</>];
 
-
-
-let root = createRoot(document.getElementById('root')!)
-render()
+let root = createRoot(document.getElementById("root")!);
+render();
 
 export default function render() {
-	root = createRoot(document.getElementById('root')!)
-	root.render(
-		(<><header><h1>3D SLOPERO</h1></header>
-			<main>
-				<fieldset>
-					<legend>
-						options for the graph
-					</legend>
+  root = createRoot(document.getElementById("root")!);
+  root.render(
+    <>
+      <header>
+        <h1>3D SLOPERO</h1>
+      </header>
+      <main>
+        <fieldset>
+          <legend>options for the graph</legend>
 
-					<label htmlFor="equationText" >Write equation to graph</label>
-					<input id='equationText' type="text" placeholder="y = 2x + 6z" />
-					<label htmlFor="variableAmount" >amount of iteration</label>
-					<input type="number" name="variableAmount" id="variableAmount" placeholder='10' />
-					<button onClick={() => {
-						let equationText: HTMLInputElement = document.querySelector('#equationText')!
-						let variableAmount: HTMLInputElement = document.querySelector('#variableAmount')!
-						generateSlope(equationText.value, variableAmount.value)
-					}}>apply</button>
-					<output ></output>
-				</fieldset>
-				<fieldset id='valueContainer'>
-					<h2>values</h2>
-					<ul>
-					{window.xyzContainer}
-					</ul>
-				</fieldset>
-				{window.jsxContainer}
-			</main >
-		</>)
-	)
+          <label htmlFor="equationText">Write equation to graph</label>
+          <input id="equationText" type="text" placeholder="y = 2x + 6z" />
+          <label htmlFor="variableAmount">amount of iteration</label>
+          <input
+            type="number"
+            name="variableAmount"
+            id="variableAmount"
+            placeholder="10"
+          />
+          <button
+            onClick={() => {
+              const equationText: HTMLInputElement =
+                document.querySelector("#equationText")!;
+              const variableAmount: HTMLInputElement =
+                document.querySelector("#variableAmount")!;
+              generateSlope(equationText.value, variableAmount.value);
+            }}
+          >
+            apply
+          </button>
+          <output></output>
+        </fieldset>
+        <fieldset id="valueContainer">
+          <h2>values</h2>
+          <ul>{window.xyzContainer}</ul>
+        </fieldset>
+        {window.jsxContainer}
+      </main>
+    </>,
+  );
 }
-
